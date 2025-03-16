44ce946b1dd73c8c66f8bec9d59ded3648c303f9
AugustoCVS <guty.vertolis@gmail.com>
2025-03-16 17:03:27 -0300

    chore: update folders name

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Header } from "./components/Header/header.component"
+import { Header } from "./components/header/header.component"
 import { ElementRoutes } from "./routes"
 import { useLocation } from 'react-router-dom'
 
